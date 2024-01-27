62fcd184c940363553c71acba2221f79e6c25ee6
Timo Wilhelm <mail@timowilhelm.com>
2024-01-27 10:41:51 +0100

    refactor error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createPrivateKey } from 'node:crypto';
 import { execSync } from 'child_process';
-import { info, setFailed, getInput, setOutput, setSecret } from '@actions/core';
+import { info, setFailed, getInput, setOutput, setSecret, error } from '@actions/core';
 import { getOctokit } from '@actions/github';
 import { request } from '@octokit/request';
 
@@ -29,16 +29,17 @@ async function findInstallationId(
 
   const appAuthentication = await auth({ type: 'app' });
 
-  const octokit = getOctokit(appAuthentication.token);
+  const { token } = appAuthentication;
+  const octokit = getOctokit(token);
   const installations = await octokit.rest.apps.listInstallations();
 
   if (installations.data.length === 0) {
-    setFailed('No installations found');
+    error('No installations found');
     return null;
   }
 
   if (installations.data.length > 1) {
-    setFailed(
+    error(
       `Detected ${installations.data.length} installations. Please provide an 'installation-id' input.`,
     );
     return null;
@@ -49,61 +50,65 @@ async function findInstallationId(
 }
 
 const run = async () => {
-  const appId = getInput('app-id');
-  const privateKeyInput = getInput('private-key');
+  try {
+    const appId = getInput('app-id');
+    const privateKeyInput = getInput('private-key');
 
-  const customRequest = request.defaults({
-    request: {
-      agent: getHttpsProxyAgent(),
-    },
-  });
+    const customRequest = request.defaults({
+      request: {
+        agent: getHttpsProxyAgent(),
+      },
+    });
 
-  // Only PKCS#8 is supported - https://github.com/gr2m/universal-github-app-jwt#readme
-  const privateKey = createPrivateKey(privateKeyInput).export({
-    type: 'pkcs8',
-    format: 'pem',
-  }).toString();
+    // Only PKCS#8 is supported - https://github.com/gr2m/universal-github-app-jwt#readme
+    const privateKey = createPrivateKey(privateKeyInput)
+      .export({
+        type: 'pkcs8',
+        format: 'pem',
+      })
+      .toString();
 
-  const installationId =
-    getInput('installation-id') || (await findInstallationId(appId, privateKey, customRequest));
+    const installationId =
+      getInput('installation-id') || (await findInstallationId(appId, privateKey, customRequest));
 
-  if (!installationId) {
-    setFailed('Failed to acquire installation ID');
-    return;
+    if (!installationId) {
+      throw Error('Failed to acquire installation ID');
+    }
+
+    setOutput('installation-id', installationId);
+    info(`Installation ID: ${installationId}`);
+
+    const installationAuth = createAppAuth({
+      appId,
+      privateKey,
+      installationId,
+      request: customRequest,
+    });
+
+    const installationAuthentication = await installationAuth({
+      type: 'installation',
+    });
+
+    const { token } = installationAuthentication;
+
+    setSecret(token);
+    setOutput('access-token', token);
+
+    if (getInput('set-git-credentials') !== 'true') {
+      info('Skipping git credential configuration');
+      return;
+    }
+    const command = `git config --global url."https://x-access-token:${token}@github.com/".insteadOf "https://github.com/"`;
+    execSync(command);
+
+    info('Git credentials configured successfully');
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error.message);
+    } else {
+      setFailed('An unexpected error occurred');
+    }
   }
-
-  setOutput('installation-id', installationId);
-  info(`Installation ID: ${installationId}`);
-
-  const installationAuth = createAppAuth({
-    appId,
-    privateKey,
-    installationId,
-    request: customRequest,
-  });
-
-  const installationAuthentication = await installationAuth({
-    type: 'installation',
-  });
-
-  const token = installationAuthentication.token;
-
-  if (!token) {
-    setFailed('Failed to acquire installation token');
-    return;
-  }
-
-  setSecret(token);
-  setOutput('access-token', token);
-
-  if (getInput('set-git-credentials') !== 'true') {
-    info('Skipping git credential configuration');
-    return;
-  }
-  const command = `git config --global url."https://x-access-token:${token}@github.com/".insteadOf "https://github.com/"`;
-  execSync(command);
-
-  info('Git credentials configured successfully');
 };
 
 run();
